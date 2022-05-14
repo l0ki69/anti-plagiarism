b4da4cfc1fa9461613212c0c48cf15439649df95
l0ki69 <lekha.titarenko@mail.ru>
2022-05-15 02:10:02 +0300

    [update] add % and message about result

--- main.py
+++ main.py
@@ -20,19 +20,21 @@ if __name__ == '__main__':
     file_name_1 = args[1]
     file_name_2 = args[2]
 
+    # file_name_1 = "text_1.txt"
+    # file_name_2 = "text_2.txt"
+
     text_1 = open_file(file_name_1)
     text_2 = open_file(file_name_2)
-    #
-    #
+
     # text_1 = "Конференция состоится завтра по адресу академии"
     # text_2 = "Завтра по адресу академии состоится конференция"
 
     # text_1 = "Солнце светит ярко"
     # text_2 = "Солнце свитит ярко"
 
-    result_percent = Plahiarismhandler.handler(text_1, text_2)
+    result = Plahiarismhandler.handler(text_1, text_2)
 
-    print(result_percent)
+    print(result)
 
     with open('output.out', 'w') as out:
-        out.write(f"result_percent = {result_percent}")
+        out.write(f"result_percent = {result[0]}\n{result[1]}")
