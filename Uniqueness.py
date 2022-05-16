fcd90510e2eb458917f425c65ff8803e1ceb6eb1
l0ki69 <lekha.titarenko@mail.ru>
2022-05-16 03:43:12 +0300

    [update] Added new methods for calculating similarity

--- Uniqueness.py
+++ Uniqueness.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List ,Dict
 
 
 class Uniqueness:
@@ -13,3 +13,22 @@ class Uniqueness:
         uniqueness_percent = len(set(shingles[0]) & set(shingles[1])) / len(set(shingles[0]))
 
         return float(uniqueness_percent)
+
+    @classmethod
+    def uniqueness_document(cls, verifiable_document: Dict, phrase_compare: List[Dict]) -> Dict:
+        uniqueness_percent = {}
+
+        if phrase_compare:
+            for doc in phrase_compare:
+                union = set(verifiable_document['hashes']) & set(doc['hashes'])
+                percent = len(union) / len(set(verifiable_document['hashes']))
+                uniqueness_percent[doc['doc_id']] = {'percent': (percent) * 100, "hashes": list(union)}
+
+                return {'document_id': verifiable_document['doc_id'], 'result': uniqueness_percent}
+        else:
+            return cls.uniqueness_document_empty(verifiable_document)
+    @classmethod
+    def uniqueness_document_empty(cls, verifiable_document: Dict):
+        return {'document_id': verifiable_document['doc_id'],
+                'result': {verifiable_document['doc_id']:
+                               {'percent': 100, "hashes": list(verifiable_document['hashes'])}}}
