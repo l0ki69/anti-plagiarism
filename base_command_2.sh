bb28329e81ef93f600800e446fb6ae62fd12cc5b
l0ki69 <lekha.titarenko@mail.ru>
2022-06-07 17:58:24 +0300

    [update] rm command.sh and update README.md

--- base_command_2.sh
+++
@@ -1,7 +0,0 @@
-python3.8 main.py index_documents 10002 10003
-
-# action - index_documents или 2
-# documents_id - [10002, 10003] это список документов которые мы хотим переиндексировать
-
-# Запуск переиндексации списка документов в общем случае выглядит:
-# python3.8 main.py 2 document_id1 document_id2 document_id3 ....
--- base_command_3.sh
+++
@@ -1,6 +0,0 @@
-python3.8 main.py index_all_documents
-
-# action - index_documents или 3
-
-# Запуск переиндексации всех документов находящихся в таблицу in_pages в общем случае выглядит:
-# python3.8 main.py 3
